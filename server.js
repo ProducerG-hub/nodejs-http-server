7408a5557e5df7b619906020fa69be1f3906a7ce
gwamaka mwakabuta <gwamakamwakabuta@gmail.com>
2026-04-27 11:47:26 +0300

    Adding the contact url on the http-server

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const http = require('http'); // the module that used for creating a SERVER loca
 const server = http.createServer((req,res)=>{
     res.writeHead(200,{'content-type':'text/plain'}); // the server response will be in plain text
     if(req.url === '/'){
-        res.end('This is http server created by MLUE');
+        res.end('Welcome to our server');
     }
        else if(req.url === '/mlue'){
         res.end('We are 1.Developers\n2.Graphics Designers\n3.Software Engineers\n4.Freelancers');
+    }
+    else if(req.url === '/contact'){
+        res.end('You can contact us at mluetechnologytz@gmail.com');
     }
     else{
         res.statusCode = 404;
@@ -15,6 +18,6 @@ const server = http.createServer((req,res)=>{
 });
 
 server.listen(3000,() => {
-    console.log('sever is listening at http://localhost:3000');
+    console.log('server is listening at http://localhost:3000');
     // Open on your browser http://localhost:3000 to view what the server is responding with
 });
